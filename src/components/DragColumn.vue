71692e41a94596e257e3382c9b4bc1694bd4751e
TheOne001 <paul.agbogun@gmail.com>
2025-06-10 23:34:20 +0100

    feat: Create DragItem component for a more modular codebase.

--- src/components/DragColumn.vue
+++ src/components/DragColumn.vue
@@ -2,26 +2,16 @@
   <div class="w-82 h-fit min-h-32 bg-indigo-400 rounded-md text-white">
     <div class="border-b-3 flex justify-between items-center px-2 py-1 h-14 border-b-gray-600">
       <p class="text-xl">{{ header }}</p>
-      <button class="bg-emerald-500 px-2 py-1 rounded-lg">+</button>
+      <button class="bg-emerald-500 px-2 py-1 rounded-lg cursor-pointer">+</button>
     </div>
     <ul class="pt-2">
-      <li
-        v-for="(item, index) in items"
-        :key="index"
-        class="px-4 py-7 hover:bg-indigo-500 transition-all cursor-grab rounded-lg flex justify-between"
-      >
-        {{ item }}
-        <div class="flex gap-1 text-black items-center">
-          <X class="w-5 h-5 cursor-pointer hover:text-emerald-500" />
-          <Pen class="w-5 h-5 cursor-pointer hover:text-emerald-500" />
-        </div>
-      </li>
+      <DragItem v-for="(item, index) in items" :item="item" :key="index" />
     </ul>
   </div>
 </template>
 
 <script setup>
-import { X, Pen } from 'lucide-vue-next'
+import DragItem from './DragItem.vue'
 defineProps({
   header: String,
   items: Array,
---
+++ src/components/DragItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li
+    class="px-4 py-7 hover:bg-indigo-500 transition-all cursor-grab rounded-lg flex justify-between"
+  >
+    {{ item }}
+    <div class="flex gap-1 text items-center">
+      <X class="w-5 h-5 cursor-pointer hover:text-emerald-500" />
+      <Pen class="w-5 h-5 cursor-pointer hover:text-emerald-500" />
+    </div>
+  </li>
+</template>
+
+<script setup>
+import { X, Pen } from 'lucide-vue-next'
+defineProps({
+  item: String,
+})
+</script>
