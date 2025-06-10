b381ac7fa790f681cafd64c1a657a41d8b5326a7
TheOne001 <paul.agbogun@gmail.com>
2025-06-10 22:41:24 +0100

    feat: create new navbar component.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import logo from '@/assets/logo.png'
+</script>
+
+<template>
+  <nav
+    class="bg-emerald-500 text-white h-15 flex items-center justify-between px-3 mx-5 rounded-lg"
+  >
+    <div class="flex gap-2">
+      <img :src="logo" class="w-7 h-7 hover:scale-110 transition-all" />
+      <p class="text-xl font-bold">Restate</p>
+    </div>
+    <div class="flex gap-7">
+      <a class="text-lg hover:-translate-y-2 transition-all cursor-pointer">Home</a>
+      <a class="text-lg hover:-translate-y-2 transition-all cursor-pointer">Boards</a>
+      <a class="text-lg hover:-translate-y-2 transition-all cursor-pointer">Pricing</a>
+      <a class="text-lg hover:-translate-y-2 transition-all cursor-pointer">Contact</a>
+    </div>
+    <div>
+      <button class="bg-indigo-400 px-3 py-2 rounded-lg">Create Account</button>
+    </div>
+  </nav>
+</template>
