6af80a4705b99502e2a866a8b8ef29543adf0d28
TheOne001 <paul.agbogun@gmail.com>
2025-06-10 23:20:50 +0100

    feat: Add ui for Drag column.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import DragColumn from './components/DragColumn.vue'
+
+const items = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5']
 </script>
 
 <template>
-  <div class="bg-gray-300 min-h-screen pt-6">
+  <div class="bg-gray-300 min-h-screen pt-6 flex flex-col gap-8">
     <Navbar />
+    <div class="flex items-center justify-center gap-5">
+      <DragColumn header="Table 1" :items="items" />
+      <DragColumn header="Table 1" :items="items" />
+      <DragColumn header="Table 1" :items="items" />
+    </div>
   </div>
 </template>
